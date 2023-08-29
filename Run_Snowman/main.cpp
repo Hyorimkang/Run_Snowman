ee9947b5a6dfdf2da4825a3cccb73c4609e3ff9b
강효림 <103922285+Hyorimkang@users.noreply.github.com>
2023-08-29 15:16:24 +0900

    [:sparkles:] 창 띄우기

---
+++ Run_Snowman/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <SFML/Graphics.hpp>
+using namespace sf;
+
+int main() {
+	RenderWindow window(VideoMode(1000, 500), "Run Snowman"); //창만들기
+
+	while (window.isOpen()) {
+		Event e;
+		while (window.pollEvent(e))
+		{
+			//윈도우의 x를 눌렀을 때 창이 닫아지도록 
+			if (e.type == Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.display();
+	}
+
+	return 0;
+}
