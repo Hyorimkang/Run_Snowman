87ab2af453117fd2c88bc2789dd1d7004865bbcb
강효림 <103922285+Hyorimkang@users.noreply.github.com>
2023-09-12 15:08:40 +0900

    [:bulbs:] 점프 부분 코드 분석

--- Run_Snowman/game.cpp
+++ Run_Snowman/game.cpp
@@ -1,7 +1,8 @@
 #include <SFML/Graphics.hpp>
 using namespace sf;
 
-#define DIR_RIGHT 0
+#define DIR_RIGHT	0
+#define DIR_LEFT	1
 
 class Snowman {
 public:
@@ -11,13 +12,16 @@ public:
 	float jumpHeight_;  // 점프 높이
 	float jumpSpeed_;   // 점프 속도
 	bool isJumping_;    // 현재 점프 중인지 여부
+	int gravity_;		//중력
 	RectangleShape sprite_;	//눈사람
 };
 
 void Game() {
 	const int WIDTH = 1000;
 	const int HEIGHT = 500;
-	const int BLOCK_SIZE = 1;
+	const int BLOCK_SIZE = 50;
+	const int G_WIDTH = WIDTH / BLOCK_SIZE;
+	const int G_HEIGHT = HEIGHT / BLOCK_SIZE;
 
 	Texture backgound;
 	Texture charactor;
@@ -49,8 +53,7 @@ void Game() {
 	snowman_.setPosition(30, 230);
 
 	//snowman 돌아오는 위치 설정
-	Vector2f initialPosition(30, 230);
-	snowman_.setPosition(snowman.x_, snowman.y_);
+	snowman_.setPosition(snowman.x_* BLOCK_SIZE, snowman.y_*BLOCK_SIZE);
 
 
 	Clock clock;
@@ -62,19 +65,22 @@ void Game() {
 				window.close();
 		}
 
-		//오른쪽으로 움직이기
+		//방향 설정
 		if (Keyboard::isKeyPressed(Keyboard::Right)) {
-			snowman.x_ += 5;
-			snowman.y_ = 230;
-			snowman_.setPosition(snowman.x_, snowman.y_);
+			snowman.dir_ = DIR_RIGHT;
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Left)) {
+			snowman.dir_ = DIR_LEFT;
 		}
 
-		//왼쪽으로 움직이기
-		if (Keyboard::isKeyPressed(Keyboard::Left)) {
-			snowman.x_ -= 5;
-			snowman.y_ = 230;
-			snowman_.setPosition(snowman.x_, snowman.y_);
+		//바운더리 밖으로 못 나가게
+		if (snowman.dir_ == DIR_RIGHT && snowman.x_ < 900) {
+			snowman.x_ += 10;
 		}
+		else if (snowman.dir_ == DIR_LEFT && snowman.x_ > 0){
+			snowman.x_ -= 10;
+		}
+
 
 		//space bar 누르면 isJumping 활성화
 		if (Keyboard::isKeyPressed(Keyboard::Space)) {
@@ -82,33 +88,33 @@ void Game() {
 			clock.restart();
 		}
 
-		//점프모션
 		if (snowman.isJumping_) {
-			float elapsedTime = clock.getElapsedTime().asSeconds();
-			float jumpDistance = snowman.jumpSpeed_ * elapsedTime;
+			float elapsedTime = clock.getElapsedTime().asSeconds();	//현재 경과 시간
+			float jumpDistance = snowman.jumpSpeed_ * elapsedTime;	//현재 시간에 따라 올라간 높이 
 
-			if (jumpDistance <= snowman.jumpHeight_) {
+			//snowman 위로 올리기 
+			if (jumpDistance <= snowman.jumpHeight_) { 
 				snowman_.move(0.0f, -jumpDistance);
 			}
+			//점프가 끝났으므로 isJumping_ 비활성화 시키기
 			else {
 				snowman.isJumping_ = false;
-				clock.restart();
+				clock.restart(); //clock 재시작
 			}
-		} 
-		else {
+		}
+		else {  // snowman이 점프상태가 아닐 때만 실행 
 			Vector2f currentPosition = snowman_.getPosition();
-			if (currentPosition.y < initialPosition.y) {
+			if (currentPosition.y < 230) {
 				// 중력 적용하여 떨어지기
 				float elapsedTime = clock.getElapsedTime().asSeconds();
 				float fallDistance = snowman.jumpSpeed_ * elapsedTime;
 				snowman_.move(0.0f, fallDistance);
 			}
 			else {
-				// 제자리로 돌아오면 초기 위치로 설정
+				//위치 업데이트
 				snowman_.setPosition(snowman.x_, snowman.y_);
 			}
 		}
-		
 
 		window.clear();
 		window.draw(game_bg);
