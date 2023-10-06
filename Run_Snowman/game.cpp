9c9889c0b1d53cad4b1193e891ebe78583c88fc6
강효림 <103922285+Hyorimkang@users.noreply.github.com>
2023-10-06 09:33:25 +0900

    :sparkles: 창 띄우고 이미지 로드하기

--- Run_Snowman/game.cpp
+++ Run_Snowman/game.cpp
@@ -4,14 +4,43 @@ using namespace sf;
 #include <iostream>
 using namespace std;
 
+#define WIDTH 1000
+#define HEIGHT 500
 
 class Snowman {
 public:
-	
+	int x_;
+	int y_;
 };
 
 void Game() {
-	
+	Snowman snowman;
+
+	//창 만들기
+	RenderWindow window(VideoMode(WIDTH, HEIGHT), "Run Snowman");
+	//1초 동안 처리하는 횟수 설정
+	window.setFramerateLimit(60);
+
+	//이미지 로드
+	Texture background;
+	Texture charactor;
+	background.loadFromFile("img/game_bg.png");
+	charactor.loadFromFile("img/snowman.png");
+
+
+	while (window.isOpen()) {
+		Event e;
+		while (window.pollEvent(e))
+		{
+			//윈도우의 x를 눌렀을 때 창이 닫아지도록 
+			if (e.type == Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.display();
+	}
+
+	return 0;
 }
 
 
