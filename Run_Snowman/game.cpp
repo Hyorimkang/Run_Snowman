2615bb1825b53ec019cdc1bbd193a5975e5abc96
강효림 <103922285+Hyorimkang@users.noreply.github.com>
2023-10-10 14:12:02 +0900

    :sparkles: 메인 화면 이미지 보이기

--- Run_Snowman/game.cpp
+++ Run_Snowman/game.cpp
@@ -103,7 +103,5 @@ void Game() {
 
 
 //Test 목적 : main과 연결 후 삭제하기
-int main() {
-	Game();
-}
+
 
--- Run_Snowman/main.cpp
+++ Run_Snowman/main.cpp
@@ -2,24 +2,18 @@
 #include <assert.h>
 using namespace sf;
 
+#define WIDTH 1000
+#define HEIGHT 500
 
 int main() {
-	const int WIDTH = 1000;
-	const int HEIGHT = 500;
-
-	Texture texture;
-	Sprite img_bg;
-	Sprite game_bg;
-	 
-	//창만들기
+	// 창만들기
 	RenderWindow window(VideoMode(WIDTH, HEIGHT), "Run Snowman");
-	 
-	//파일 불러오기
-	texture.loadFromFile("img/main_bg.png");
+
+	Texture back;
+	back.loadFromFile("img/main_bg.png");
 
 	//이미지 넣기
-	img_bg.setTexture(texture);
-	img_bg.setTextureRect(IntRect(0, 0, WIDTH, HEIGHT));
+	Sprite game_bg = Sprite(back);
 
 	while (window.isOpen()) {
 		Event e;
@@ -29,20 +23,13 @@ int main() {
 			if (e.type == Event::Closed)
 				window.close();
 
-			//키 눌렀을 때 이벤트
-			if (Keyboard::isKeyPressed(Keyboard::Right)) {
-				// TODO : 키를 누르면 화면전환이 되도록 하기
-				//배경이미지
-				texture.loadFromFile("img/game_bg.png");
+			if (e.type == Event::MouseButtonPressed && e.mouseButton.button == Mouse::Left) {
 
-				//배경 이미지 삽입
-				game_bg.setTexture(texture);
-				game_bg.setTextureRect(IntRect(0, 0, WIDTH, HEIGHT));
 			}
 		}
-	}
 		window.clear();
-		window.draw(img_bg);
 		window.draw(game_bg);
 		window.display();
+	}
+		
 }
